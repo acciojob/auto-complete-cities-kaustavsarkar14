c41e018d26ddb4e39bb1d39c422969be0257dff8
kaustavsarkar14 <sarkar.kaustav14@gmail.com>
2023-11-23 19:21:13 +0530

    test

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,45 +1,49 @@
 import React, { useState } from 'react'
 
-const Search = ({cities}) => {
+const Search = ({ cities }) => {
     const [filteredCities, setFilteredCities] = useState([])
-    const [searchTerm, setSearchTerm] = useState('') 
-    function filterCities(search){
-        if(search=='') return
-        setFilteredCities(cities.filter(el=>el.toLowerCase().includes(search.toLowerCase())))
+    const [searchTerm, setSearchTerm] = useState('')
+    function filterCities(search) {
+        if (search == '') {
+            setFilteredCities([])
+            return
+        }
+        setFilteredCities(cities.filter(el => el.toLowerCase().includes(search.toLowerCase())))
     }
-    function handleChange(e){
+    function handleChange(e) {
         filterCities(e.target.value)
         setSearchTerm(e.target.value)
     }
-    function handleClick(listText){
+    function handleClick(listText) {
         setSearchTerm(listText)
         setFilteredCities([])
     }
-  return (
-    <div style={{
-        display:"flex",
-        flexDirection:"column"
-    }} >
-        <input value={searchTerm} type="text" onChange={handleChange} />
-        <ul style={{
-            listStyle:"none",
-            display:"flex",
-            flexDirection:"column",
-            gap:"5px",
-            padding:"0"
+
+    return (
+        <div style={{
+            display: "flex",
+            flexDirection: "column"
         }} >
-            {
-                filteredCities.map(el=>{
-                    return(
-                        <li style={{backgroundColor:"lightgrey"}} 
-                        onClick={e=>handleClick(e.target.innerText)}
-                        >{el}</li>
-                    )
-                })
-            }
-        </ul>
-    </div>
-  )
+            <input value={searchTerm} type="text" onChange={handleChange} />
+            <ul style={{
+                listStyle: "none",
+                display: "flex",
+                flexDirection: "column",
+                gap: "5px",
+                padding: "0"
+            }} >
+                {
+                    filteredCities.map((el,i) => {
+                        return (
+                            <li key={i} style={{ backgroundColor: "lightgrey" }}
+                                onClick={e => handleClick(e.target.innerText)}
+                            >{el}</li>
+                        )
+                    })
+                }
+            </ul>
+        </div>
+    )
 }
 
 export default Search
